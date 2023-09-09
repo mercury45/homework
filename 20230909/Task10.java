7e3e5b6e33a185e32bb773ffcf7fd94143214b71
Zharenov Nikita <lking1231@mail.ru>
2023-09-09 22:34:14 +0300

    Solved 10.2 and 10.3 tasks

--- 20230909/Task10.java
+++ 20230909/Task10.java
@@ -2,9 +2,27 @@ public class Task10 {
 	public static void main(String [] args){
 		int n = 4;
 		String sym = "*";
+		
 		for (int i = 1; i <= n; i++){
 			for (int j = 1; j <= i; j++){
 				System.out.print(sym);
+			}
+			System.out.println();
+		} 
+		System.out.println();
+		for (int i = 1; i <= n; i++) {
+			for (int j = 1; j <= (n+1)-i; j++){
+				System.out.print(sym);
+			}
+			System.out.println();
+		}
+		System.out.println();
+		for (int i = 1; i <= n; i++){
+			for (int j = 1; j <= (n-i); j++) {
+				System.out.print(" ");
+			}
+			for (int k = 1; k <= i; k++) {
+				System.out.print("*");
 			}
 			System.out.println();
 		}
