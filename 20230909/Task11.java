631779389e438886fbf9b0073dc114d105b0d9dd
Zharenov Nikita <lking1231@mail.ru>
2023-09-10 16:18:29 +0300

    [NOT READY] Added new file 'Task11.java'

---
+++ 20230909/Task11.java
@@ -0,0 +1,20 @@
+public class Task11 {
+	public static void main(String[] args){
+		int n = 5;
+		final int BASE_OF_ONE = 2*n-1;
+		/*
+		length of base of one triangle is 2*n-1. length of base of all triangles is 2*(2*n-1)
+		height of 2 triangles is 2*n
+		the centre of picture is (base of all triangles - 1) / 2
+		Making first triangle
+		*/
+		final int BASE_OF_TRIANGLES = 2*CORE_LENGTH
+		final int CENTRE = (BASE_OF_TRIANGLES-1)/2
+		for (int i = 1; i <= n; i++) {
+			for (int j1 = 1; j1 <= CENTRE; j1++) {
+				
+			}
+			System.out.println();
+		}
+	}
+}
